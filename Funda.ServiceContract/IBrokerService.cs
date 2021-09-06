c678ce9279813df837475c3723d9bfce9fd2d850
Sina Vatanparast <43779430+sina-vatanparast@users.noreply.github.com>
2021-09-06 17:40:59 +0430

    Add files via upload

---
+++ Funda.ServiceContract/IBrokerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Funda.DataContract;
+
+
+namespace Funda.ServiceContract
+{
+    /// <summary>
+    /// Get all objects in the city with the condition (with gardern and without) and returns the top n brokers provided the most objects
+    /// </summary>
+    /// <param name="count">the number of top brokers</param>
+    /// <param name="city">the name of the city</param>
+    /// <param name="garden">indicates object with a garden or without</param>
+    /// <returns>a list of top n brokers provided the most objects </returns>
+    public interface IBrokerService
+    {
+        Task<List<BrokerModel>> GetTopOnes(int count, string city, bool garden = false);
+    }
+}
---
+++ Funda.ServiceContract/IObjectApiHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Funda.DataContract;
+
+namespace Funda.ServiceContract
+{
+    public interface IObjectApiHttpClient
+    {
+        Task<IReadOnlyCollection<ObjectModel>> Execute(string city, bool garden);
+    }
+}
